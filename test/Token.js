49f8f92bd078305d38fcca43239cc159f6c8d578
Dmytro Petrenko <petrenko.d28@gmail.com>
2022-11-21 23:26:39 +0200

    create exchange

--- test/Token.js
+++ test/Token.js
@@ -1,7 +1,5 @@
 const { expect } = require('chai');
 const {ethers} = require('hardhat')
-// import { expect } from 'chai';
-// import { ethers } from 'hardhat'
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
