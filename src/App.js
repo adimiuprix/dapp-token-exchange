58baa90dbc57dfa80ef846a7ffba24bcf49d0226
Dmytro Petrenko <petrenko.d28@gmail.com>
2022-12-13 03:39:24 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
   loadTokens,
   loadExchange
 } from './store/interactions';
+import Navbar from './components/Navbar';
 
 function App() {
 
@@ -20,9 +21,18 @@ function App() {
     const provider = loadProvider(dispatch);
     // Fetch current network's chainId (e.g. hardhat: 31337, kovan: 42)
     const chainId = await loadNetwork(provider, dispatch);
-    // Fetch current account & balance from Metamsk
-    const account = await loadAccount(provider, dispatch);
-    console.log('First account: ', account);
+
+    // Reload page when network changes
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload();
+    });
+
+    // Fetch current account & balance from Metamsk when changed
+    window.ethereum.on('accountsChanged', () => {
+      loadAccount(provider, dispatch);
+    })
+    
+    // console.log('First account: ', account);
     // Load token smart contract
     const DApp = config[chainId].DApp;
     const mETH = config[chainId].mETH;
@@ -35,13 +45,13 @@ function App() {
 
   useEffect(() => {
     loadBlockChainData();
-  }, []);
+  });
 
   return (
     <div>
+      <Navbar />
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
-
         </section>
         <section className='exchange__section--right grid'>
 
